e5aa4adcd774d75d3df97ca12b16e7ab9cb73571
Fauzy <haliffauzy45@gmail.com>
2024-04-04 13:17:08 +0700

    change code

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return(
-        <div className="container mx-auto bg-gray-600 text-white font-bold mt-2 p-2">
+        <div className="container mx-auto bg-gray-600 text-white font-bold p-2">
              <div className="container mx-auto text-center">
                 <p>&copy; Copyright 2024, FauzyDev.</p>
             </div>
