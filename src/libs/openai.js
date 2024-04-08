8a568f784f097721fb63a98dff5554a32f0f2e0b
Fauzy <haliffauzy45@gmail.com>
2024-04-08 16:28:01 +0700

    change code

--- src/libs/openai.js
+++ src/libs/openai.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai"
 
 const openai = new OpenAI({
-    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    apiKey: process.env.SECRET_OPENAI_API_KEY,
     dangerouslyAllowBrowser: true
 
   })
